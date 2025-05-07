51e2a6482be555cd8898eb1f6dcd83965a9d8853
adnan_ <kuranadnan1@gmail.com>
2025-05-07 18:30:07 +0300

    Update AddWord.Designer.cs

--- LoopLearn/Frontend/AddWord.Designer.cs
+++ LoopLearn/Frontend/AddWord.Designer.cs
@@ -43,21 +43,21 @@
             // 
             // tbxEngWordName
             // 
-            tbxEngWordName.Location = new Point(314, 202);
+            tbxEngWordName.Location = new Point(310, 216);
             tbxEngWordName.Name = "tbxEngWordName";
             tbxEngWordName.Size = new Size(230, 23);
             tbxEngWordName.TabIndex = 0;
             // 
             // tbxTurWordName
             // 
-            tbxTurWordName.Location = new Point(314, 255);
+            tbxTurWordName.Location = new Point(310, 269);
             tbxTurWordName.Name = "tbxTurWordName";
             tbxTurWordName.Size = new Size(230, 23);
             tbxTurWordName.TabIndex = 1;
             // 
             // tbxSample
             // 
-            tbxSample.Location = new Point(314, 307);
+            tbxSample.Location = new Point(310, 321);
             tbxSample.Name = "tbxSample";
             tbxSample.Size = new Size(230, 23);
             tbxSample.TabIndex = 2;
@@ -67,7 +67,7 @@
             lblEngWordName.AutoSize = true;
             lblEngWordName.BackColor = Color.Transparent;
             lblEngWordName.ForeColor = Color.White;
-            lblEngWordName.Location = new Point(217, 205);
+            lblEngWordName.Location = new Point(213, 219);
             lblEngWordName.Name = "lblEngWordName";
             lblEngWordName.Size = new Size(91, 15);
             lblEngWordName.TabIndex = 3;
@@ -78,7 +78,7 @@
             lblTurWordName.AutoSize = true;
             lblTurWordName.BackColor = Color.Transparent;
             lblTurWordName.ForeColor = Color.White;
-            lblTurWordName.Location = new Point(198, 258);
+            lblTurWordName.Location = new Point(194, 272);
             lblTurWordName.Name = "lblTurWordName";
             lblTurWordName.Size = new Size(110, 15);
             lblTurWordName.TabIndex = 4;
@@ -89,7 +89,7 @@
             lblSample.AutoSize = true;
             lblSample.BackColor = Color.Transparent;
             lblSample.ForeColor = Color.White;
-            lblSample.Location = new Point(226, 310);
+            lblSample.Location = new Point(222, 324);
             lblSample.Name = "lblSample";
             lblSample.Size = new Size(82, 15);
             lblSample.TabIndex = 5;
@@ -97,7 +97,7 @@
             // 
             // btnAddWord
             // 
-            btnAddWord.Location = new Point(314, 412);
+            btnAddWord.Location = new Point(310, 426);
             btnAddWord.Name = "btnAddWord";
             btnAddWord.Size = new Size(230, 28);
             btnAddWord.TabIndex = 6;
@@ -107,7 +107,7 @@
             // 
             // btnChoosePicture
             // 
-            btnChoosePicture.Location = new Point(314, 374);
+            btnChoosePicture.Location = new Point(310, 388);
             btnChoosePicture.Name = "btnChoosePicture";
             btnChoosePicture.Size = new Size(99, 23);
             btnChoosePicture.TabIndex = 7;
@@ -117,7 +117,7 @@
             // 
             // btnChooseVoice
             // 
-            btnChooseVoice.Location = new Point(445, 374);
+            btnChooseVoice.Location = new Point(441, 388);
             btnChooseVoice.Name = "btnChooseVoice";
             btnChooseVoice.Size = new Size(99, 23);
             btnChooseVoice.TabIndex = 8;
@@ -127,7 +127,7 @@
             // pctSamplePicture
             // 
             pctSamplePicture.BackColor = Color.Gray;
-            pctSamplePicture.Location = new Point(588, 202);
+            pctSamplePicture.Location = new Point(584, 216);
             pctSamplePicture.Name = "pctSamplePicture";
             pctSamplePicture.Size = new Size(236, 238);
             pctSamplePicture.SizeMode = PictureBoxSizeMode.Zoom;
